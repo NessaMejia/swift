6300b84f2cd7bd39842d365d7548b2fd7c27afa7
Vanessa Mejia <vanessamejia@MacBook-Pro-de-Vanessa.local>
2016-12-09 10:32:20 -0600

    modificaciones, no tomar cero como divisible entre 5 y numero par

--- JuegoDeMemoria.playground/Contents.swift
+++ JuegoDeMemoria.playground/Contents.swift
@@ -5,13 +5,13 @@ import UIKit
 var str = "Juego de Memoria"
 
 for i in 0 ... 100 {
-    if i % 5 == 0 {
+    if i % 5 == 0 && i > 0 {
         print("\n\(i) Bingo !!!")
     }
-    if i % 2 == 0 {
+    if i % 2 == 0 && i > 0 {
         print("\n\(i) Par !!!")
     }
-    else if i % 2 > 0 {
+    else if i % 2 != 0 {
        print("\n\(i) Impar !!!")
     }
     
